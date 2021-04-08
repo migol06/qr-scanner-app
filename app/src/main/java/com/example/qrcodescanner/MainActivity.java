5af6b7252faf1dc925f5d5090288e8a2cbfda6a7
Miguel Rosal <miguelrosal12@gmail.com>
2021-04-08 20:40:48 +0800

    Login and Registration Complete

--- app/src/main/java/com/example/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.qrcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private TextView signup;
+    private EditText username, password;
+    private Button button;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth=FirebaseAuth.getInstance();
+        mLoadingBar = new ProgressDialog(MainActivity.this);
+
+
         initViews();
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,11 +44,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkCredentials()){
+                    mLoadingBar.setTitle("Login");
+                    mLoadingBar.setMessage("Please wait, checking your credentials");
+                    mLoadingBar.setCanceledOnTouchOutside(false);
+                    mLoadingBar.show();
+
+                    mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        mLoadingBar.dismiss();
+                                    }else{
+                                        Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                        mLoadingBar.dismiss();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+
 
     }
 
+
+    private boolean checkCredentials() {
+        String name=username.getText().toString();
+        String pass1=password.getText().toString();
+
+        if(name.equals("")){
+            showError(username, "Please input an email");
+            return false;
+        }
+
+
+        if (pass1.equals("")){
+            showError(password, "Input Password");
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
+    }
+
+
     private void initViews() {
         signup=findViewById(R.id.Signup);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.editTextPassword);
+        button=findViewById(R.id.button);
 
     }
 }
--- app/src/main/java/com/example/qrcodescanner/signupActivity.java
+++ app/src/main/java/com/example/qrcodescanner/signupActivity.java
@@ -1,17 +1,27 @@
 package com.example.qrcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class signupActivity extends AppCompatActivity {
 
     private EditText name, email, password, reenterpass;
     private Button button;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +29,50 @@ public class signupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         initViews();
+
+
+        //firebase
+        mAuth=FirebaseAuth.getInstance();
+        mLoadingBar = new ProgressDialog(signupActivity.this);
+
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View view) {
                 if(checkCredentials()){
-                    Toast.makeText(signupActivity.this, "Registered", Toast.LENGTH_SHORT).show();
-                }
+                    mLoadingBar.setTitle("Registration");
+                    mLoadingBar.setMessage("Please wait, checking your credentials");
+                    mLoadingBar.setCanceledOnTouchOutside(false);
+                    mLoadingBar.show();
+
+                    //Firebase Auth
+                    mAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(signupActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(signupActivity.this, HomeActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }else {
+                                Toast.makeText(signupActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                mLoadingBar.dismiss();
+                            }
+                        }
+                    });
+
+
+
+                } //if bracket
             }
         });
-
 
 
 
     }
 
     private boolean checkCredentials() {
+
         String username=name.getText().toString();
         String useremail=email.getText().toString();
         String pass1=password.getText().toString();
@@ -54,11 +93,19 @@ public class signupActivity extends AppCompatActivity {
             return false;
         }
 
+        if (pass1.length() < 6){
+            showError(password, "Weak Password");
+            return false;
+        }
+
         if (pass2.equals("") || !pass2.equals(pass1)){
             showError(reenterpass, "Password not match");
             return false;
         }
         return true;
+
+
+
 
     }
 
@@ -69,10 +116,11 @@ public class signupActivity extends AppCompatActivity {
 
 
     private void initViews() {
-        name=findViewById(R.id.editTextTextName);
+        name=findViewById(R.id.editTxtUsername);
         email=findViewById(R.id.editTextTextEmail);
         password=findViewById(R.id.password);
         reenterpass=findViewById(R.id.editTextTextReEnterPassword);
         button=findViewById(R.id.button);
+
     }
 }
