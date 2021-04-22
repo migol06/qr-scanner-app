19019a7f63ddaac04b74a88afe61de4bd22253f5
Miguel Rosal <miguelrosal12@gmail.com>
2021-04-22 22:10:44 +0800

    Working SearchView

--- app/src/main/java/com/example/qrcodescanner/listActivity.java
+++ app/src/main/java/com/example/qrcodescanner/listActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +19,7 @@ public class listActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private infoAdapter adapter;
+    private FirebaseRecyclerOptions<uploadInformation> options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,7 @@ public class listActivity extends AppCompatActivity {
     }
 
     private void setUpRecyclerView() {
-        FirebaseRecyclerOptions<uploadInformation> options = new
+        options = new
                 FirebaseRecyclerOptions.Builder<uploadInformation>()
                 .setQuery(FirebaseDatabase.getInstance().getReference().child("Information"), uploadInformation.class)
                 .build();
@@ -52,5 +58,57 @@ public class listActivity extends AppCompatActivity {
 
     private void initViews() {
         recyclerView = findViewById(R.id.recycler_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.searchmenu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                orig(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                search(s);
+                //search(s);
+                return false;
+            }
+        });
+
+
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void orig(String s) {
+        options = new FirebaseRecyclerOptions.Builder<uploadInformation>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("Information")
+                        .orderByChild("info").startAt(s).endAt(s + "\uf8ff"),uploadInformation.class)
+                .build();
+
+        adapter = new infoAdapter(options);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void search(String s) {
+        options = new FirebaseRecyclerOptions.Builder<uploadInformation>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("Information")
+                .orderByChild("time").startAt(s).endAt(s + "\uf8ff"),uploadInformation.class)
+                .build();
+
+        adapter = new infoAdapter(options);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
