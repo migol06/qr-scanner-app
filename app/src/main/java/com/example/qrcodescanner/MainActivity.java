54e08bce5a627722fc804edb76244aab24b52c41
Miguel Rosal <miguelrosal12@gmail.com>
2021-05-01 20:30:12 +0800

    add titles and change app name

--- app/src/main/java/com/example/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Information Scanner");
 
         mAuth=FirebaseAuth.getInstance();
         mLoadingBar = new ProgressDialog(MainActivity.this);
--- app/src/main/java/com/example/qrcodescanner/signupActivity.java
+++ app/src/main/java/com/example/qrcodescanner/signupActivity.java
@@ -27,7 +27,7 @@ public class signupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+        setTitle("Register User");
         initViews();
 
 
